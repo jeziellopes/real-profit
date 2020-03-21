7b02f6fa735cf61cadc4782fe2d6985873c28a33
jlllo <jeziellcarvalho@gmail.com>
2020-03-21 15:43:29 -0300

    add eslint packages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import App from '../src/App';
+import App from './App';
 
 test('renders real profit application title', () => {
   const { getByText } = render(<App />);
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>Real Profit</h1>
-  );
-}
+export default class App extends React.Component {
+  componentDidMount() {
+    return [];
+  }
 
-export default App;
+  render() {
+    return (
+      <h1>Real Profit</h1>
+    );
+  }
+}
