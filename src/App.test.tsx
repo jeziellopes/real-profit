caf565004b973a0c86ef3e30031ba746217a472c
jlllo <jeziellcarvalho@gmail.com>
2020-03-21 13:09:11 -0300

    add typescript react app

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import App from '../src/App';
+
+test('renders real profit application title', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/real profit/i);
+  expect(linkElement).toBeInTheDocument();
+});
