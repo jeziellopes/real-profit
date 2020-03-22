3506a2357eba1ef21cdfdf84ecc5531d29e6d049
jlllo <jeziellcarvalho@gmail.com>
2020-03-22 02:17:52 -0300

    add redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Link, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import ProfitSimulator from './pages/ProfitSimulator';
 import Route from './routes/Route';
+import store from './store';
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/simulador">Profit Simulator</Link>
-            </li>
-          </ul>
-        </nav>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/simulador">Profit Simulator</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/simulador" exact component={ProfitSimulator} />
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/simulador" exact component={ProfitSimulator} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+
   );
 }
